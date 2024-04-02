6fc33379b481d1fac626a3990546bdde97370220
alpacaJin <alpacaJin9@gmail.com>
2024-04-02 13:37:04 -0700

    dc motor functions tested & finished

--- controller.ino
+++ controller.ino
@@ -4,48 +4,19 @@
 
 #include <Servo.h>                  // Servo library to control servo motors
 
-// LED Pin Definitions for status and direction indication
-#define led_FR 14                   // LED Front Right  pin A0 for Arduino Uno
-#define led_FL 15                   // LED Front Left   pin A1
-#define led_BR 16                   // LED Back Left    pin A2
-#define led_BL 17                   // LED Back Right   pin A3
-
-// Motor Driver Control (DC) Pins
-#define motor_IN1 2                 // Motor Driver Right IN1
-#define motor_IN2 3                 // Motor Driver Right IN2
-#define motor_IN3 4                 // Motor Driver Left IN3
-#define motor_IN4 5                 // Motor Driver Left IN4
-#define ENA 10                      // Motor Driver Enable A for right side speed control
-#define ENB 11                      // Motor Driver Enable B for left side speed control
-
 // Servo & Bluetooth Module pin definitions for steering & communication
 #define servoPin 5                  // Servo motor pin
-// #define BT_RX 7                     // Bluetooth module RX pin for receiving data
-// #define BT_TX 8                  // Bluetooth module TX pin for sending data
+#define BT_RX 7                     // Bluetooth module RX pin for receiving data
+#define BT_TX 8                     // Bluetooth module TX pin for sending data
 
 Servo myServo;                      // Create servo object
 int servoVal;                       // Int for storing servo angle
 
-int command; 			                  // Int to store app command from smartphone app
-int speedCar = 100; 	              // Default speed
-int speedCoeff = 4;                 // Coefficient for speed sensitivity adjustment
+int command; 			            // Int to store app command from smartphone app
+int speedCar = 96; 	                // Default speed
 
 
 void setup() {
-    // Initialize LED
-    pinMode(led_FR, OUTPUT);
-    pinMode(led_FL, OUTPUT);
-    pinMode(led_BR, OUTPUT);
-    pinMode(led_BL, OUTPUT);
-    
-	// Initialize Motor Driver pins
-    pinMode(motor_IN1, OUTPUT);
-    pinMode(motor_IN2, OUTPUT);
-    pinMode(motor_IN3, OUTPUT);
-    pinMode(motor_IN4, OUTPUT);
-    pinMode(ENA, OUTPUT);
-    pinMode(ENB, OUTPUT);
-
     // Initialize servo
     myServo.attach(servoPin);
 
@@ -68,29 +39,27 @@ void stopCar();
 
 // Move car forward
 void goAhead() {
-    digitalWrite(motor_IN1, HIGH);
-    digitalWrite(motor_IN2, LOW);
-    digitalWrite(motor_IN3, HIGH);
-    digitalWrite(motor_IN4, LOW);
-    analogWrite(ENA, speedCar);
-    analogWrite(ENB, speedCar);
+    myServo.write(speedCar);        // Initialize forward ~96
+
+    speedCar += 10;
+    myServo.write(speedCar);        // Increment 10 -> 106
+    Serial.print(speedCar);
 }
 
 
 // Move car backward
 void goBack() {
-    digitalWrite(motor_IN1, LOW);   // Reverse polarity compared to going forward
-    digitalWrite(motor_IN2, HIGH);
-    digitalWrite(motor_IN3, LOW);
-    digitalWrite(motor_IN4, HIGH);
-    analogWrite(ENA, speedCar);
-    analogWrite(ENB, speedCar);
+    speedCar = 88;
+    myServo.write(speedCar);        // Initialize reverse ~88
+ 
+    speedCar -= 5;
+    myServo.write(speedCar);        // Decrement 5 -> 83
 }
 
 
 // Turn right
 void goRight() {
-    // 135 degrees for a right turn
+    // 0 degrees for a right turn
     servoVal = 0;
     myServo.write(servoVal);             
 }
@@ -98,116 +67,82 @@ void goRight() {
 
 // Turn left
 void goLeft() {
-    // 45 degrees for a left turn
-    servoVal = 90;
+    // 180 degrees for a left turn
+    servoVal = 180;
     myServo.write(servoVal);               
 }
 
+
 void zero() {
-  servoVal = 45;
-  myServo.write(servoVal);
+    // 90 degrees for center
+    servoVal = 90;
+    myServo.write(servoVal);
 }
 
 
 // Diagonal movements & speed adjustments
 void goAheadRight() {
     // Increase speed on left & decrease on right
-    analogWrite(ENA, speedCar/speedCoeff);
-    analogWrite(ENB, speedCar);
 
     // Set both sides to move forward
-    digitalWrite(motor_IN1, HIGH);
-    digitalWrite(motor_IN2, LOW);
-    digitalWrite(motor_IN3, HIGH);
-    digitalWrite(motor_IN4, LOW);
 }
 
 void goAheadLeft() {
     // Increase speed on right & decrease on left
-    analogWrite(ENA, speedCar);
-    analogWrite(ENB, speedCar/speedCoeff);
 
     // Set both sides to move forward
-    digitalWrite(motor_IN1, HIGH);
-    digitalWrite(motor_IN2, LOW);
-    digitalWrite(motor_IN3, HIGH);
-    digitalWrite(motor_IN4, LOW);
 }
 
 void goBackRight() {
     // Decrease speed on left & increase on right
-    analogWrite(ENA, speedCar/speedCoeff);
-    analogWrite(ENB, speedCar);
     
     // Set both sides to move backwards
-    digitalWrite(motor_IN1, LOW);
-    digitalWrite(motor_IN2, HIGH);
-    digitalWrite(motor_IN3, LOW);
-    digitalWrite(motor_IN4, HIGH);
 }
 
 void goBackLeft() {
     // Decrease speed on right & increase on left
-    analogWrite(ENA, speedCar);
-    analogWrite(ENB, speedCar/speedCoeff);
 
     // Set both sides to move backwards
-    digitalWrite(motor_IN1, LOW);
-    digitalWrite(motor_IN2, HIGH);
-    digitalWrite(motor_IN3, LOW);
-    digitalWrite(motor_IN4, HIGH);
 }
 
 
 // Stop
 void stopCar() {
-    digitalWrite(motor_IN1, LOW);
-    digitalWrite(motor_IN2, LOW);
-    digitalWrite(motor_IN3, LOW);
-    digitalWrite(motor_IN4, LOW);
-    digitalWrite(ENA, 0);
-    digitalWrite(ENB, 0);
+    speedCar = 96;
+    myServo.write(speedCar);
 }
 
 
 // Loop
 void loop() {
     if (Serial.available() > 0) {
-      // Read the incoming byte:
-      int command = Serial.parseInt(); // Parses an integer from incoming serial data
-      
-      // Echo the command back to the serial terminal (optional):
-      Serial.print("Command received: ");
-      Serial.println(command);
+        // Read the incoming byte
+        int command = Serial.parseInt(); // Parses an integer from incoming serial data
+        
+        // Echo the command back to the serial terminal (optional):
+        Serial.print("Command received: ");
+        Serial.println(command);
 
-      switch (command) {
-        case 0: zero();           break;    // servo
-        case 1: goAhead();        break;    // dc
-        case 2: goBack();         break;    // dc
-        case 3: goRight();        break;    // servo
-        case 4: goLeft();         break;    // servo
-        case 5: goAheadRight();   break;    // dc
-        case 6: goAheadLeft();    break;    // dc
-        case 7: goBackRight();    break;    // dc
-        case 8: goBackLeft();     break;    // dc
+        switch (command) {
+            // servo
+            case 1: zero();           break;   
+            case 2: goRight();        break;    
+            case 3: goLeft();         break;
 
-        // Speed controls
-        case 10: speedCar = 100;   break;
-        case 11: speedCar = 115;   break;
-        case 12: speedCar = 130;   break;
-        case 13: speedCar = 145;   break;
-        case 14: speedCar = 160;   break;
-        case 16: speedCar = 175;   break;
-        case 16: speedCar = 190;   break;
-        case 17: speedCar = 205;   break;
-        case 18: speedCar = 220;   break;
-        case 19: speedCar = 235;   break;
-        case 20: speedCar = 255;   break;
-        default: stopCar();       break;
-      }
+            // dc
+            case 4: goAhead();        break;   
+            case 5: goBack();         break;
 
-      delay(10);
+            case 6: stopCar();        break;
+
+            // case 5: goAheadRight();   break;    // dc
+            // case 6: goAheadLeft();    break;    // dc
+            // case 7: goBackRight();    break;    // dc
+            // case 8: goBackLeft();     break;    // dc
+
+            delay(10);
     }
+}
 
     /* Preliminary Bluetooth module code */
     // if (Serial.available() > 0) {
@@ -224,18 +159,6 @@ void loop() {
     //         case 'G': goAheadLeft();    break;
     //         case 'J': goBackRight();    break;
     //         case 'H': goBackLeft();     break;
-
-    //         case '0': speedCar = 100;   break;
-    //         case '1': speedCar = 115;   break;
-    //         case '2': speedCar = 130;   break;
-    //         case '3': speedCar = 145;   break;
-    //         case '4': speedCar = 160;   break;
-    //         case '5': speedCar = 175;   break;
-    //         case '6': speedCar = 190;   break;
-    //         case '7': speedCar = 205;   break;
-    //         case '8': speedCar = 220;   break;
-    //         case '9': speedCar = 235;   break;
-    //         case 'q': speedCar = 255;   break;
     //     }
     // }
 }
